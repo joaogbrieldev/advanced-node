7f6043d626ffbfcb9202c4d751bf0dffce23e5d4
João Figueiredo <joao.ggwp@outlook.com>
2025-11-16 12:45:53 -0300

    test: change test strategy to use jest mocks

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,34 +1,23 @@
-import {
-  ILoadFacebookUserApi,
-  LoadFacebookUserApi,
-} from "@/domain/data/contracts/apis";
 import { FacebookAuthenticationService } from "@/domain/data/services/facebook-authentication";
 import { AuthenticationError } from "@/domain/errors/authentication";
 
-class LoadFacebookUserApiSpy implements ILoadFacebookUserApi {
-  token?: string;
-  result = undefined;
-  callsCount = 0;
-  async loadUser(
-    params: LoadFacebookUserApi.Params
-  ): Promise<LoadFacebookUserApi.Result> {
-    this.token = params.token;
-    this.callsCount++;
-    return this.result;
-  }
-}
-
 describe("FacebookAuthentication", () => {
   it("should call LoadFacebookUserApi with correct params", async () => {
-    const loadFacebookUserApi = new LoadFacebookUserApiSpy();
+    const loadFacebookUserApi = {
+      loadUser: jest.fn(),
+    };
     const sut = new FacebookAuthenticationService(loadFacebookUserApi);
     await sut.perform({ token: "any_token" });
-    expect(loadFacebookUserApi.token).toBe("any_token");
-    expect(loadFacebookUserApi.callsCount).toBe(1);
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledWith({
+      token: "any_token",
+    });
+    expect(loadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1);
   });
   it("should return AuthenticationError if LoadFacebookUserApi returns undefined", async () => {
-    const loadFacebookUserApi = new LoadFacebookUserApiSpy();
-    loadFacebookUserApi.result = undefined;
+    const loadFacebookUserApi = {
+      loadUser: jest.fn(),
+    };
+    loadFacebookUserApi.loadUser.mockResolvedValue(undefined);
     const sut = new FacebookAuthenticationService(loadFacebookUserApi);
     const authResult = await sut.perform({ token: "any_token" });
     expect(authResult).toEqual(new AuthenticationError());
