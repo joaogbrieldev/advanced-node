cc55f000f960b4d69819f5064db548ee153174f9
João Figueiredo <joao.ggwp@outlook.com>
2025-11-20 09:52:51 -0300

    test: 100 coverage tests

--- tests/domain/models/acess-token.spec.ts
+++ tests/domain/models/acess-token.spec.ts
@@ -5,6 +5,10 @@ describe("AcessToken", () => {
     const sut = new AcessToken("any_token");
     expect(sut).toEqual({ value: "any_token" });
   });
+  it("should create a new access token using factory method", () => {
+    const sut = AcessToken.create("factory_token");
+    expect(sut).toEqual({ value: "factory_token" });
+  });
   it("should expire in 30 minutes", () => {
     expect(AcessToken.expirationInMs).toBe(1800000);
   });
