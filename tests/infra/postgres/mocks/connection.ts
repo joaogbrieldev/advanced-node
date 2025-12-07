0d91681d593636bd56e489c2b5da14c6366f4d88
João Figueiredo <joao.ggwp@outlook.com>
2025-12-07 15:21:12 -0300

    move inMemoryDb creation to its owns file

---
+++ tests/infra/postgres/mocks/connection.ts
@@ -0,0 +1,11 @@
+import { newDb } from "pg-mem";
+
+export const makeFakeDb = async (entities?: any[]): Promise<any> => {
+  const db = newDb();
+  const connection = await db.adapters.createTypeormConnection({
+    type: "postgres",
+    entities: entities ?? ["src/infra/postgres/entities/index.ts"],
+  });
+  await connection.synchronize();
+  return db;
+};
